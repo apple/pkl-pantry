d9571cdce407bb7f1687f81b945a4bfd24ce017d
Lily Ballard <lilyball@apple.com>
2024-06-04 11:56:51 -0700

    [pkl.lua] Add class `Parser` and enhance `Renderer` (#60)
    * Add class `Parser` that can parse a static subset of Lua
    * Update `Renderer` to applying converters to mapping entry keys
    * Rename LuaIdentifier, LuaKeyword, and LuaRenderDirective to drop the leading "Lua", and deprecate renamed typealiases

---
+++ packages/pkl.lua/tests/fixtures/comment.lua
@@ -0,0 +1,15 @@
+-- This is a copy of the sample file from the Parser doc comment
+--[[
+This file has a header comment.
+]]
+foo="bar"
+count=2
+-- line comment here
+enable=true
+frob=nil
+ports={80, 443}
+ips={
+  localhost = "127.0.0.1";
+  ["example.com"] = "93.184.215.14";
+}
+_ENV[" "]="space"
