5f21e92e3a26b9e1ba283e4084f50d062a8a4fe4
Dan Chao <dan.chao@apple.com>
2025-12-02 17:21:43 -0800

    Address PR feedback
    * Remove older versions of each typed action; only keep latest around
    * Make `context` available on the Workflow module itself
    * Adjust doc comments to wrap at 100 chars

--- scripts/generate-github.sh
+++ scripts/generate-github.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pkl eval --project-dir "$SCRIPT_DIR" "$SCRIPT_DIR"/generate-github-actions.pkl -m "$SCRIPT_DIR"/..
 pkl eval --project-dir "$SCRIPT_DIR" "$SCRIPT_DIR"/generate-github-catalog.pkl -m "$SCRIPT_DIR"/..
 
-cd "SCRIPT_DIR/.."
+cd "$SCRIPT_DIR/.."
 
 ./gradlew spotlessApply
